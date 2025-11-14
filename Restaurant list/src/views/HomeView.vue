f2afbfb0884eeedfe23ecfba24e26cccb47c00f0
Teng-Wei-Hsu <s0595751@htw-berlin.de>
2025-11-14 10:30:05 +0100

    Milestone 2 V4

--- Restaurant list/src/views/HomeView.vue
+++ Restaurant list/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/RestaurantList.vue'
+import RestaurantList from "@/components/RestaurantList.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <RestaurantList />
   </main>
 </template>
